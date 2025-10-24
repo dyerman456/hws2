b7d6f630def4b419cd510c1eb72d51c0d1887e9f
dyerman456 <dyerman456@gmail.com>
2025-10-24 07:46:45 +0200

    solve HW11

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route, Navigate} from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -14,13 +14,8 @@ export const PATH = {
 function Pages() {
   return (
     <div>
-      {/*Routes выбирает первый подходящий роут*/}
       <Routes>
-        {/*роутинг будут писать студенты*/}
-        {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-        {/*<Route ...*/}
         <Route path={"/"} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
-        {/*<Route pa*/}
 
         {/*роуты для /pre-junior, /junior, /junior-plus*/}
         <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
@@ -28,14 +23,6 @@ function Pages() {
         <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
         <Route path="*" element={<Error404/>}/>
-
-        {/*<Route ...*/}
-        {/*<Route ...*/}
-        {/*<Route ...*/}
-
-        {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-        {/*<Route ...*/}
-
       </Routes>
     </div>
   )
